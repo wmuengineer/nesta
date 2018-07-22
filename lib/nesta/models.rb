0bf7da75456ec1cd582e199de9d710be89483766
Graham Ashton <graham@effectif.com>
2018-07-22 12:52:52 +0100

    Upgrade to tilt 2.x
    Some of the code in tilt 1.4 generates warnings when run under Ruby 2.4.
    
    For example:
    
        /path/to/.gem/ruby/2.4.2/gems/tilt-1.4.1/lib/tilt/haml.rb:55:in
        `block in precompiled_postamble':
        Haml::Engine#precompiled_method_return_value at
        /path/to/.rubies/ruby-2.4.2/lib/ruby/2.4.0/forwardable.rb:157
        forwarding to private method
        Haml::Compiler#precompiled_method_return_value
    
    Upgrading to tilt 2.0 fixes the problem.
    
    tilt 2.0 automatically requires gems that are used to process templates
    (e.g. `maruku`), which tilt 1.4 didn't do. I've therefore had to update
    the code that registers the Markdown template handlers in
    `lib/nesta/models.rb`.
    
    We ignore `LoadError` as we don't require any of these handlers to be
    present, as `rdiscount` is used by default. It might be sensible to stop
    requiring `rdiscount` here, and allowing each site to include whichever
    Markdown handler is preferred in its `Gemfile`, but that's out of scope
    of this commmit.
    
    Were we to move to specifying the default Markdown processor in each
    site's `Gemfile` I suspect it would also make sense to check whether
    Haml's inline Markdown feature works with any Markdown processor, and
    update the docs accordingly if it does. I suspect that Haml's inline
    Markdown processing might be one of the reasons why `rdiscount` is hard
    coded as a default.

--- lib/nesta/models.rb
+++ lib/nesta/models.rb
@@ -2,11 +2,16 @@ require 'time'
 
 require 'rdiscount'
 
-Tilt.register Tilt::MarukuTemplate, 'mdown', 'md'
-Tilt.register Tilt::KramdownTemplate, 'mdown', 'md'
-Tilt.register Tilt::BlueClothTemplate, 'mdown', 'md'
-Tilt.register Tilt::RDiscountTemplate, 'mdown', 'md'
-Tilt.register Tilt::RedcarpetTemplate, 'mdown', 'md'
+def register_template_handler(class_name, *extensions)
+  Tilt.register Tilt.const_get(class_name), *extensions
+rescue LoadError
+end
+
+register_template_handler :MarukuTemplate, 'mdown', 'md'
+register_template_handler :KramdownTemplate, 'mdown', 'md'
+register_template_handler :BlueClothTemplate, 'mdown', 'md'
+register_template_handler :RDiscountTemplate, 'mdown', 'md'
+register_template_handler :RedcarpetTemplate, 'mdown', 'md'
 
 module Nesta
   class HeadingNotSet < RuntimeError; end
